8ee7e5c709a7a25d4a532f603f718eadc1342394
L2X9 <65819758+L2X9@users.noreply.github.com>
2020-09-27 05:26:25 -0400

    Initial commit

---
+++ src/main/java/org/l2x9/l2x9core/Main.java
@@ -0,0 +1,17 @@
+package org.l2x9.l2x9core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
