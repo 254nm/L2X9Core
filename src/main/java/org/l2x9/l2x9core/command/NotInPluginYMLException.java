0409eae9cd4ebe28ec332425db6f38d94c2064bb
cube-builder <sebastiantmk@gmail.com>
2021-04-03 20:06:17 -0400

    change package name to leeesexploitfixer

--- src/main/java/org/l2x9/l2x9core/command/NotInPluginYMLException.java
+++
@@ -1,8 +0,0 @@
-package org.l2x9.l2x9core.command;
-
-public class NotInPluginYMLException extends Exception {
-
-    public NotInPluginYMLException(String message) {
-        super(message);
-    }
-}
